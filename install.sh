3a930b299fe9cd7ff7fdaf7a73d7798f9f352432
Colin <cfinn68916@gmail.com>
2022-10-17 18:55:19 -0500

    add man page

--- install.sh
+++ install.sh
@@ -2,5 +2,7 @@ wget {source link} --output-file /bin/notebook
 chmod +x /bin/notebook
 mkdir ~/.notes
 wget {example notes file} --output-file ~/.notes/help
+wget {man-page} --output-file /usr/share/man/man1/notebook.1
+gzip /usr/share/man/man1/notebook.1
 echo 'Notebook has been setup. Run "notebook help" to get started'
 
